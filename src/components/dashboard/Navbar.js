eff9672d4b06f764d49864892e6804323b380293
Modupe Adebayo <modupeadebayo001@gmail.com>
2020-01-09 17:28:25 +0100

    fixed missing bell icon

--- src/components/dashboard/Navbar.js
+++ src/components/dashboard/Navbar.js
@@ -10,36 +10,36 @@ import {
   NavImg,
   NavCont
 } from './styles/navbar';
-import './Dashboard.css';
 
 export const Navbar = () => {
   return (
     <NavbarWrapper>
       <NavCont>
         <Navbrand>
-          <NavbrandText>TransMonitor</NavbrandText>
-        </Navbrand>
+          <NavbrandText> TransMonitor </NavbrandText>{' '}
+        </Navbrand>{' '}
         <NavInput type='text' placeholder='&#xF002;  Search' />
-      </NavCont>
+      </NavCont>{' '}
       <NavItemContainer>
         <div className='nav-items ml-auto'>
           <NavMenu>
-            <NavMenuItem>Support</NavMenuItem>
-            <NavMenuItem>FAQ</NavMenuItem>
+            <NavMenuItem> Support </NavMenuItem>{' '}
+            <NavMenuItem> FAQ </NavMenuItem>{' '}
             <NavMenuItem>
-              <i className='fa fa-bell badge'></i>
-            </NavMenuItem>
-            <NavMenuItem>Support</NavMenuItem>
+              <i className='fa fa-bell'>
+                <span className='badges'>1</span>
+              </i>
+            </NavMenuItem>{' '}
             <NavMenuItem>
-              <small className='text-muted float-right'>Hello</small> <br></br>{' '}
-              Oluwaleke Ojo
-            </NavMenuItem>
+              <small className='text-muted float-right'> Hello </small>{' '}
+              <br></br> Oluwaleke Ojo{' '}
+            </NavMenuItem>{' '}
             <NavMenuItem>
               <NavImg src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50' />
-            </NavMenuItem>
-          </NavMenu>
-        </div>
-      </NavItemContainer>
+            </NavMenuItem>{' '}
+          </NavMenu>{' '}
+        </div>{' '}
+      </NavItemContainer>{' '}
     </NavbarWrapper>
   );
 };
